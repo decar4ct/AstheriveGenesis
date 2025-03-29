048aaaab8bafdb4e4dfa70229b25df6339a43763
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-29 13:51:57 +0700

    mnr spling mistek

--- src/astherivegen/content/Elaris/blocks/ElarisEnv.java
+++ src/astherivegen/content/Elaris/blocks/ElarisEnv.java
@@ -17,7 +17,7 @@ public class ElarisEnv {
             eonstoneFloor, eonstoneLightlyErodedFloor, eonstoneErodedFloor,
             eonstoneWall, eonstoneErodedWall,
             alyogelFloor,
-            eonstoneBoulder,eonstoneErodedBoulde,
+            eonstoneBoulder,eonstoneErodedBoulder,
             sporfloreTree;
     public static void load() {
         {
