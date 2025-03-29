73c78f9edded736bfe63ea5aab5bedf273e0b474
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-29 15:05:37 +0700

    i dontknow what im doing

---
+++ src/astherivegen/world/blocks/defense/ConnectedWall.java
@@ -0,0 +1,16 @@
+package astherivegen.world.blocks.defense;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+
+public class ConnectedWall extends Wall {
+    public ConnectedWall(String name){
+        super(name);
+    }
+
+    public class ConnectedWallBuild extends WallBuild {
+        
+    }
+}
