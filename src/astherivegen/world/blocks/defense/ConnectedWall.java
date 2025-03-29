bb441163f23534662183376fc77f54b18db3ad54
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-29 19:10:48 +0700

    Update ConnectedWall.java

--- src/astherivegen/world/blocks/defense/ConnectedWall.java
+++ src/astherivegen/world/blocks/defense/ConnectedWall.java
@@ -14,21 +14,38 @@ import mindustry.world.Tile;
 public class ConnectedWall extends Wall {
     public TextureRegion sideRegion;
     public ConnectedWall(String name){
-        super(name);
-    }
+         super(name);
+    
     @Override
     public void load(){
         super.load();
         sideRegion = Core.atlas.find(name+"-side");
+    }
+    //permanently borrowed from payloadblock.java :troll:
+    public static boolean blends(Building build){
+        Building accept = build.nearby(Geometry.d4(direction).x, Geometry.d4(direction).y);
+        return accept != null && (accept.block.size == size
+            && Math.me+"-side");
+    }
+    //permanently borrowed from payloadblock.java :troll:
+    public static boolean blends(Building build){
+        Building accept = build.nearby(Geometry.d4(direction).x, Geometry.d4(direction).y);
+        return accept != null &&
+
+            //if size is the same, block must either be facing this one, or not 
+            (accept.block.size == size
+            && Math.abs(accept.tileX() - build.tileX()) % size == 0 //check alignment
+            && Math.abs(accept.tileY() - build.tileY()) % size == 0);
     }
     public class ConnectedWallBuild extends WallBuild {
         @Override
         public void draw(){
             super.draw();
             Draw.rect(region, x, y, 0);
-            Draw.z(Layer.blockOver);
             for (int i=0;i<4;i++) {
-                Draw.rect(sideRegion, x, y, 90+i*90);
+                if (blends(i)){
+                    Draw.rect(sideRegion, x, y, (i*90)-180);
+                }
             }
         }
     }
