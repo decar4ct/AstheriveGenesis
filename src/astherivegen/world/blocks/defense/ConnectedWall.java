32b11e00fc8c697faecafb8aa1ce7f012266ae48
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-29 18:39:51 +0700

    Update ConnectedWall.java

--- src/astherivegen/world/blocks/defense/ConnectedWall.java
+++ src/astherivegen/world/blocks/defense/ConnectedWall.java
@@ -8,9 +8,7 @@ import arc.math.*;
 import arc.util.*;
 import mindustry.world.blocks.defense.*;
 import mindustry.gen.Building;
-import mindustry.graphics.Drawf;
-import mindustry.graphics.Layer;
-import mindustry.graphics.Pal;
+import mindustry.graphics.*;
 import mindustry.world.Tile;
 
 public class ConnectedWall extends Wall {
@@ -18,18 +16,19 @@ public class ConnectedWall extends Wall {
     public ConnectedWall(String name){
         super(name);
     }
-
+    @Override
+    public void load(){
+        super.load();
+        sideRegion = Core.atlas.find(name);
+    }
     public class ConnectedWallBuild extends WallBuild {
-        @Override
-        public void load(){
-            super.load();
-            sideRegion = Core.atlas.find(name);
-        }
         @Override
         public void draw(){
             super.draw();
             Draw.rect(region, x, y, 0);
-            Draw.rect(sideRegion, x, y, 0);
+            for (int i=0;i<4;i++) {
+                Draw.rect(sideRegion, x, y, 90+i*90);
+            }
         }
     }
 }
