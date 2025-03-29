ce3e040ab3119b52388f826b020e8b3928a8a172
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-30 05:17:46 +0700

    i dont expect this to work

--- src/astherivegen/world/blocks/defense/ConnectedWall.java
+++ src/astherivegen/world/blocks/defense/ConnectedWall.java
@@ -18,44 +18,16 @@ import static mindustry.Vars.*;
 
 public class ConnectedWall extends Wall {
     //AWFUL
-    public TextureRegion none
-    public TextureRegion r;
-    public TextureRegion d;
-    public TextureRegion l;
-    public TextureRegion u;
-    public TextureRegion ru;
-    public TextureRegion dr;
-    public TextureRegion ld;
-    public TextureRegion ul;
-    public TextureRegion rl;
-    public TextureRegion du;
-    public TextureRegion rud;
-    public TextureRegion drl;
-    public TextureRegion lud;
-    public TextureRegion url;
-    public TextureRegion rdlu;
+    public TextureRegion[] connectedRegions = new TextureRegion[15];
     public ConnectedWall(String name){
          super(name);
     }
     @Override
     public void load(){
         super.load();
-        none = Core.atlas.find(name+"1");
-        r = Core.atlas.find(name+"2");
-        d = Core.atlas.find(name+"3");
-        l = Core.atlas.find(name+"4");
-        u = Core.atlas.find(name+"5");
-        ru = Core.atlas.find(name+"6");
-        dr = Core.atlas.find(name+"7");
-        ld = Core.atlas.find(name+"8");
-        ul = Core.atlas.find(name+"9");
-        rl = Core.atlas.find(name+"10");
-        du = Core.atlas.find(name+"11");
-        rud = Core.atlas.find(name+"12");
-        drl = Core.atlas.find(name+"13");
-        lud = Core.atlas.find(name+"14");
-        url = Core.atlas.find(name+"15");
-        rdlu = Core.atlas.find(name+"16");
+        for (int i=0;i<15;i++){
+            connectedRegions[i]=Core.atlas.find(name+String.valueOf(i+1));
+        }
     }
     //permanently borrowed from canvasblock.java :troll:
     public class ConnectedWallBuild extends WallBuild {
@@ -76,22 +48,7 @@ public class ConnectedWall extends Wall {
         @Override
         public void draw(){
             super.draw();
-            Draw.rect(region, x, y, 0);
-            for(int i = 0; i < 4; i ++){
-                if((blending & (1 << i)) == 0){
-                    Draw.rect(i >= 2 ? sideRegion2 : sideRegion1, x, y, i * 90);
-
-                    if((blending & (1 << ((i + 1) % 4))) != 0){
-                        Draw.rect(i >= 2 ? cornerRegion2 : cornerRegion1, x, y, i * 90);
-                    }
-
-                    if((blending & (1 << (Mathf.mod(i - 1, 4)))) != 0){
-                        Draw.yscl = -1f;
-                        Draw.rect(i >= 2 ? cornerRegion2 : cornerRegion1, x, y, i * 90);
-                        Draw.yscl = 1f;
-                    }
-                }
-            }
+            Draw.rect(connectedRegions[blending], x, y, 0);
         }
     }
 }
