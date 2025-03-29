d2e9a627d80c83525200375d93faa803527a5ddf
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-29 19:14:53 +0700

    cleanup fix

--- src/astherivegen/world/blocks/defense/ConnectedWall.java
+++ src/astherivegen/world/blocks/defense/ConnectedWall.java
@@ -24,15 +24,7 @@ public class ConnectedWall extends Wall {
     //permanently borrowed from payloadblock.java :troll:
     public static boolean blends(Building build){
         Building accept = build.nearby(Geometry.d4(direction).x, Geometry.d4(direction).y);
-        return accept != null && (accept.block.size == size
-            && Math.me+"-side");
-    }
-    //permanently borrowed from payloadblock.java :troll:
-    public static boolean blends(Building build){
-        Building accept = build.nearby(Geometry.d4(direction).x, Geometry.d4(direction).y);
         return accept != null &&
-
-            //if size is the same, block must either be facing this one, or not 
             (accept.block.size == size
             && Math.abs(accept.tileX() - build.tileX()) % size == 0 //check alignment
             && Math.abs(accept.tileY() - build.tileY()) % size == 0);
