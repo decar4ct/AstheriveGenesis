fbaf6c6ffaf6b25d316dd0508b6f92d591fd3163
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-03-30 07:54:12 +0700

    still idk what im doing

---
+++ src/astherivegen/world/blocks/environment/DustyFloor.java
@@ -0,0 +1,37 @@
+package astherivegen.world.blocks.environment;
+
+import arc.Core;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.*;
+import arc.util.*;
+import arc.math.geom.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.gen.Building;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+
+import static mindustry.Vars.*;
+
+public class DustyFloor extends Floor {
+    public TextureRegion dustRegion;
+    public ConnectedWall(String name){
+         super(name);
+    }
+    @Override
+    public void load(){
+        super.load();
+         dustRegion=Core.atlas.find(name+"-dust");
+    }
+    @Override
+    public boolean updateRender(Tile tile) 
+        return true;
+}
+    @Override
+    public void renderUpdate(UpdateRenderState tile) {
+        if(tile.block()!=Blocks.air) {
+            Draw.rect(dustRegion,tile.x,tile.y);
+        }
+}
