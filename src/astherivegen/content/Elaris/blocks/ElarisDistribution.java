295129e9ebe820432fc88b688863b6382e35062d
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-01 12:25:05 +0700

    Update ElarisDistribution.java

--- src/astherivegen/content/Elaris/blocks/ElarisDistribution.java
+++ src/astherivegen/content/Elaris/blocks/ElarisDistribution.java
@@ -21,7 +21,7 @@ public class ElarisDistribution {
             {
                 itemTube = new ItemTube("item-tube"){{
                     requirements(Category.distribution, with(Items.copper, 1));
-                    speed = 8;
+                    speed = 0.5;
                 }};
             }
         }
