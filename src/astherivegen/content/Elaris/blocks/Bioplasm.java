66a0c51eeb2f236273ab4fd8b6add2d8b23f240a
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-02 08:32:57 +0700

    Create Bioplasm.java

---
+++ src/astherivegen/content/Elaris/blocks/Bioplasm.java
@@ -0,0 +1,28 @@
+package astherivegen.content.bioplasm;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.storage.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import mindustry.content.*;
+import astherivegen.world.blocks.bioplasm.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class Bioplasm {
+    public static Block
+            root;
+    public static void load() {
+        {
+            {
+                root = new Root("root"){{
+                    requirements(Category.distribution, with(Items.copper, 1));
+                }};
+            }
+        }
+    }
+}
