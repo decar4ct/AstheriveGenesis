817b7bb1c38b7b391fdd62a1119025b1a0873a29
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-02 08:30:10 +0700

    Create Root.java

---
+++ src/astherivegen/world/blocks/bioplasm/Root.java
@@ -0,0 +1,53 @@
+package astherivegen.world.blocks.bioplasm;
+
+import arc.Core;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.*;
+import arc.util.*;
+import arc.math.geom.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.gen.Building;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.Tile;
+
+import static mindustry.Vars.*;
+
+public class Root extends Block {
+    //AWFUL
+    public TextureRegion[] connectedRegions = new TextureRegion[16];
+    public Root(String name){
+         super(name);
+    }
+    @Override
+    public void load(){
+        super.load();
+        for (int i=0;i<16;i++){
+            connectedRegions[i]=Core.atlas.find(name+String.valueOf(i+1));
+        }
+    }
+    //permanently borrowed from canvasblock.java :troll:
+    public class RootBuild extends Building {
+        public int blending;
+        @Override
+        public void onProximityUpdate(){
+            super.onProximityUpdate();
+            blending = 0;
+            for(int i = 0; i < 4; i++){
+                if(blends(world.tile(tile.x + Geometry.d4[i].x, tile.y + Geometry.d4[i].y))){
+                    blending |= (1 << i);
+                }
+            }
+        }
+        boolean blends(Tile other){
+            return other != null && other.build != null && other.build.block == block && other.build.tileX() == other.x && other.build.tileY() == other.y;
+        }
+        @Override
+        public void draw(){
+            Draw.rect(connectedRegions[blending], x, y, 0);
+        }
+    }
+}
