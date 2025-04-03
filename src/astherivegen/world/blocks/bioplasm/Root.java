9630d6c7983dd679d2b88b5e64955ce3bfc78eba
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-03 08:52:22 +0700

    Update Root.java

--- src/astherivegen/world/blocks/bioplasm/Root.java
+++ src/astherivegen/world/blocks/bioplasm/Root.java
@@ -56,11 +56,11 @@ public class Root extends Wall {
         @Override
         public void updateTile() {
             for (int i=0;i<4;i++) {
-                Building advroot = nearby(Geometry.d4(i).x,Geometry.d4(i).y);
-                if (advroot instanceof RootBuild) {
-                    if (advroot.getPulse()>0){
-                        advroot.pulse(biopulse-1);
-                        Fx.healBlockFull.at(advroot.x, advroot.y, advroot.block.size, Color.valueOf("84f491"), advroot.block);
+                Tile advroot = tile.nearby(i)
+                if (advroot.build instanceof RootBuild) {
+                    if (advroot.block().getPulse()>0){
+                        advroot.block().pulse(biopulse-1);
+                        Fx.healBlockFull.at(advroot.x, advroot.y, advroot.block().size, Color.valueOf("84f491"), advroot.block());
                     }
                 }
             }
