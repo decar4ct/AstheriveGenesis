426fef1f739cc50385e29738e894f0baedb73cb5
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-03 17:07:13 +0700

    Update Bioplasm.java

--- src/astherivegen/content/Elaris/blocks/Bioplasm.java
+++ src/astherivegen/content/Elaris/blocks/Bioplasm.java
@@ -15,11 +15,15 @@ import static mindustry.type.ItemStack.with;
 
 public class Bioplasm {
     public static Block
-            root;
+            root,
+            pulseSource;
     public static void load() {
         {
             {
                 root = new Root("root"){{
+                    requirements(Category.distribution, with(Items.copper, 1));
+                }};
+                pulseSource = new pulseSource("pulse-source"){{
                     requirements(Category.distribution, with(Items.copper, 1));
                 }};
             }
