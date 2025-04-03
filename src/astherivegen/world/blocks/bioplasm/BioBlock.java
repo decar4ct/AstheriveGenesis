573fcd439217371733330a557d9cc5f8f05b55b1
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-04 06:44:23 +0700

    Update BioBlock.java

--- src/astherivegen/world/blocks/bioplasm/BioBlock.java
+++ src/astherivegen/world/blocks/bioplasm/BioBlock.java
@@ -49,10 +49,8 @@ public class BioBlock extends Block {
                 for (int i=0;i<4;i++) {
                     Building advroot = tile.nearbyBuild(i);
                     if (advroot instanceof BioBuilding advbuild) {
-                        if (advbuild.biopulse>=0&&!advbuild.pulsed&&biopulse>0){
-                            advbuild.nextBiopulse=Math.max(advbuild.biopulse,biopulse-1);
-                            Fx.healBlockFull.at(advbuild.x, advbuild.y, advbuild.block().size, Color.valueOf("84f491"), advbuild.block());
-                        }
+                        advbuild.nextBiopulse=Math.max(advbuild.biopulse,biopulse-1);
+                        Fx.healBlockFull.at(advbuild.x, advbuild.y, advbuild.block().size, Color.valueOf("84f491"), advbuild.block());
                     }
                 }
             }
