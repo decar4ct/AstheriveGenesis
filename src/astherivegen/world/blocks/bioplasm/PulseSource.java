23ba087cb5c32658617a0166d3b0bc8b8eeab2d1
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-03 19:56:03 +0700

    Update PulseSource.java

--- src/astherivegen/world/blocks/bioplasm/PulseSource.java
+++ src/astherivegen/world/blocks/bioplasm/PulseSource.java
@@ -25,13 +25,24 @@ public class PulseSource extends BioBlock {
     }
     public class PulseSourceBuild extends BioBuilding {
         @Override
+        public void updateTile() {
+            pulseProgress+=delta();
+            if (pulseProgress>=30f){
+                pulseProgress=0f;
+                updatePulse();
+            }
+        }
+        @Override
         public void updatePulse() {
-            for (int i=0;i<4;i++) {
-                Building advroot = tile.nearbyBuild(i);
-                if (advroot instanceof BioBuilding advbuild) {
-                    if (advbuild.biopulse>=0){
-                        advbuild.biopulse=16;
-                        Fx.healBlockFull.at(advbuild.x, advbuild.y, advbuild.block().size, Color.valueOf("84f491"), advbuild.block());
+            if (biopulse>0) {
+                for (int i=0;i<4;i++) {
+                    Building advroot = tile.nearbyBuild(i);
+                    if (advroot instanceof BioBuilding advbuild) {
+                        if (advbuild.biopulse>=0&&!advbuild.pulsed&&biopulse>0){
+                            advbuild.biopulse=16;
+                            advbuild.pulsed=true;
+                            Fx.healBlockFull.at(advbuild.x, advbuild.y, advbuild.block().size, Color.valueOf("84f491"), advbuild.block());
+                        }
                     }
                 }
             }
