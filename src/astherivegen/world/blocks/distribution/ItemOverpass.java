1639f06fc5661e9e5de899b30b2b458c03e48add
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-05 17:23:28 +0700

    Update ItemOverpass.java

--- src/astherivegen/world/blocks/distribution/ItemOverpass.java
+++ src/astherivegen/world/blocks/distribution/ItemOverpass.java
@@ -30,6 +30,35 @@ public class ItemOverpass extends DuctBridge {
         dir2=Core.atlas.find(name+"-dir2");
         side=Core.atlas.find(name+"-dir-side");
     }
+    @Override
+    public void drawBridge(int rotation, float x1, float y1, float x2, float y2, @Nullable Color liquidColor){
+        Draw.alpha(Renderer.bridgeOpacity);
+        float
+        angle = rotation==0||rotation==2?0:270; //i copied entire drawBridge() just to change this lol
+        cx = (x1 + x2)/2f,
+        cy = (y1 + y2)/2f,
+        len = Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2)) - size * tilesize;
+
+        Draw.rect(bridgeRegion, cx, cy, len, bridgeRegion.height * bridgeRegion.scl(), angle);
+        if(liquidColor != null){
+            Draw.color(liquidColor, liquidColor.a * Renderer.bridgeOpacity);
+            Draw.rect(bridgeLiquidRegion, cx, cy, len, bridgeLiquidRegion.height * bridgeLiquidRegion.scl(), angle);
+            Draw.color();
+            Draw.alpha(Renderer.bridgeOpacity);
+        }
+        if(bridgeBotRegion.found()){
+            Draw.color(0.4f, 0.4f, 0.4f, 0.4f * Renderer.bridgeOpacity);
+            Draw.rect(bridgeBotRegion, cx, cy, len, bridgeBotRegion.height * bridgeBotRegion.scl(), angle);
+            Draw.reset();
+        }
+        Draw.alpha(Renderer.bridgeOpacity);
+
+        for(float i = 6f; i <= len + size * tilesize - 5f; i += 5f){
+            Draw.rect(arrowRegion, x1 + Geometry.d4x(rotation) * i, y1 + Geometry.d4y(rotation) * i, angle);
+        }
+
+        Draw.reset();
+    }
     public class ItemOverpassBuild extends DuctBridgeBuild {
         @Override
         public void draw(){
