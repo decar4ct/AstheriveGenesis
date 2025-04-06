aade1799dea32bf901b997933fdf9230c7f6e7c0
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-06 07:17:52 +0700

    Update ElarisItems.java

--- src/astherivegen/content/Elaris/blocks/ElarisItems.java
+++ src/astherivegen/content/Elaris/blocks/ElarisItems.java
@@ -10,18 +10,18 @@ public class Items{
     public static final Seq<Item> serpuloItems = new Seq<>()
 
     public static void load(){
-        quartz = new Item("quartz", Color.valueOf("d99d73")){{
+        quartz = new Item("quartz", Color.valueOf("BCC3BF")){{
             hardness = 1;
             cost = 0.5f;
             alwaysUnlocked = true;
         }};
 
-        magnetite = new Item("magnetite", Color.valueOf("8c7fa9")){{
+        magnetite = new Item("magnetite", Color.valueOf("6C6E75")){{
             hardness = 2;
             cost = 0.7f;
         }};
       
-        polterite = new Item("polterite", Color.valueOf("53565c")){{
+        polterite = new Item("polterite", Color.valueOf("888283")){{
             hardness = 3;
             cost = 0.8f;
         }};
