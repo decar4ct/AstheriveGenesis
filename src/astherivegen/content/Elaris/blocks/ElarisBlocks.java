2ef8727c2cfc56b0a0e00b6f2fea2f65e3356eb9
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-06 14:18:15 +0700

    Update ElarisBlocks.java

--- src/astherivegen/content/Elaris/blocks/ElarisBlocks.java
+++ src/astherivegen/content/Elaris/blocks/ElarisBlocks.java
@@ -4,6 +4,7 @@ import astherivegen.content.bioplasm.*;
 public class ElarisBlocks{
     public static void load(){
         ElarisEnv.load();
+        ElarisTurrets.load();
         ElarisDefense.load();
         ElarisDistribution.load();
         ElarisStorage.load();
