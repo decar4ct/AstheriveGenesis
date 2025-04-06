bc8dbb3e2b261699e8ed87831913e5c448de25ca
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-06 08:51:20 +0700

    Update ElarisTurrets.java

--- src/astherivegen/content/Elaris/blocks/ElarisTurrets.java
+++ src/astherivegen/content/Elaris/blocks/ElarisTurrets.java
@@ -13,28 +13,38 @@ import mindustry.content.*;
 
 import static mindustry.type.ItemStack.with;
 
-fracture = new ItemTurret("fracture"){{
-            requirements(Category.turret, with(ElarisItems.quartz, 50, ElarisItems.magnetite, 40));
-            researchCost = with(ElarisItems.quartz, 100, ElarisItems.magnetite, 75);
+public class ElarisDefense {
+    public static Block
+            quartzWall;
+    public static void load() {
+        {
+            {
+                fracture = new ItemTurret("fracture"){{
+                    requirements(Category.turret, with(ElarisItems.quartz, 50, ElarisItems.magnetite, 40));
+                    researchCost = with(ElarisItems.quartz, 100, ElarisItems.magnetite, 75);
   
-            health = 700;
-            outlineColor = AGPal.outline;
-            reload = 80f;
-            inaccuracy = 2f;
-            size = 3;
-            recoil = 3f;
-            range = 18 * Vars.tilesize;
-            rotateSpeed = 3f;
-            squareSprite = false;
-            shootSound = Sounds.cannon;
+                    health = 700;
+                    outlineColor = AGPal.outline;
+                    reload = 80f;
+                    inaccuracy = 2f;
+                    size = 3;
+                    recoil = 3f;
+                    range = 18 * Vars.tilesize;
+                    rotateSpeed = 3f;
+                    squareSprite = false;
+                    shootSound = Sounds.cannon;
   
-            drawer = new DrawTurret("elaris-"){{
-                parts.add(
-                        new RegionPart("-side"){{
+                    drawer = new DrawTurret("elaris-"){{
+                        parts.add(
+                            new RegionPart("-side"){{
                             progress = PartProgress.warmup;
                             mirror = true;
                             under = false;
                             moveRot = -10;
                         }},
-            }};
-        }};
+                    }};
+                }};
+            }
+        }
+    }
+}
