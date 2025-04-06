cfd7b85e1722101fee9eaa2fe8316a0261c24e51
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-06 07:16:26 +0700

    Create ElarisItems.java

---
+++ src/astherivegen/content/Elaris/blocks/ElarisItems.java
@@ -0,0 +1,33 @@
+package astherive-gen.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class Items{
+    public static Item
+    quartz, magnetite, polterite;
+    public static final Seq<Item> serpuloItems = new Seq<>()
+
+    public static void load(){
+        quartz = new Item("quartz", Color.valueOf("d99d73")){{
+            hardness = 1;
+            cost = 0.5f;
+            alwaysUnlocked = true;
+        }};
+
+        magnetite = new Item("magnetite", Color.valueOf("8c7fa9")){{
+            hardness = 2;
+            cost = 0.7f;
+        }};
+      
+        polterite = new Item("polterite", Color.valueOf("53565c")){{
+            hardness = 3;
+            cost = 0.8f;
+        }};
+        
+        elarisItems.addAll(
+        quartz, magnetite, polterite
+        );
+    }
+}
