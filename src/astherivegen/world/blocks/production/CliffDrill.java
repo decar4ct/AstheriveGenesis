708ae3c7a00218a15bad0e44c7881d8253a74d8e
decar4ct <67582961+decar4ct@users.noreply.github.com>
2025-04-18 21:04:26 +0700

    Update CliffDrill.java

--- src/astherivegen/world/blocks/production/CliffDrill.java
+++ src/astherivegen/world/blocks/production/CliffDrill.java
@@ -54,7 +54,7 @@ public class CliffDrill extends BeamDrill {
             }
             Draw.rect(side, x, y, rotation%2==0?0:270);
 
-            if(isPayload()) return;
+            ifif(isPayload()) return;
 
             var dir = Geometry.d4(rotation);
             int ddx = Geometry.d4x(rotation + 1), ddy = Geometry.d4y(rotation + 1);
@@ -62,13 +62,72 @@ public class CliffDrill extends BeamDrill {
             for(int i = 0; i < size; i++){
                 Tile face = facing[i];
                 if(face != null){
-                    float len = Math.max(Math.abs(face.worldx() - (dir.x/2f)*tilesize), Math.abs(face.worldy() - (dir.y/2f)*tilesize;
-                                                                                                )) - size * tilesize;
-                    for(float i = 8f; i <= len + size; i += tilesize){
-                        Draw.rect(bore, face.worldx() + Geometry.d4x(rotation) * i, face.worldy() + Geometry.d4y(rotation) * i, angle);
+                    Point2 p = lasers[i];
+                    float lx = face.worldx() - (dir.x/2f)*tilesize, ly = face.worldy() - (dir.y/2f)*tilesize;
+
+                    float width = (laserWidth + Mathf.absin(Time.time + i*5 + (id % 9)*9, glowScl, pulseIntensity)) * warmup;
+
+                    Draw.z(Layer.power - 1);
+                    Draw.mixcol(glowColor, Mathf.absin(Time.time + i*5 + id*9, glowScl, glowIntensity));
+                    if(Math.abs(p.x - face.x) + Math.abs(p.y - face.y) == 0){
+                        Draw.scl(width);
+
+                        if(boostWarmup < 0.99f){
+                            Draw.alpha(1f - boostWarmup);
+                            Draw.rect(laserCenter, lx, ly);
+                        }
+
+                        if(boostWarmup > 0.01f){
+                            Draw.alpha(boostWarmup);
+                            Draw.rect(laserCenterBoost, lx, ly);
+                        }
+
+                        Draw.scl();
+                    }else{
+                        float lsx = (p.x - dir.x/2f) * tilesize, lsy = (p.y - dir.y/2f) * tilesize;
+
+                        if(boostWarmup < 0.99f){
+                            Draw.alpha(1f - boostWarmup);
+                            Drawf.laser(laser, laserEnd, lsx, lsy, lx, ly, width);
+                        }
+
+                        if(boostWarmup > 0.001f){
+                            Draw.alpha(boostWarmup);
+                            Drawf.laser(laserBoost, laserEndBoost, lsx, lsy, lx, ly, width);
+                        }
                     }
+                    Draw.color();
+                    Draw.mixcol();
+
+                    Draw.z(Layer.effect);
+                    Lines.stroke(warmup);
+                    rand.setState(i, id);
+                    Color col = face.wallDrop().color;
+                    Color spark = Tmp.c3.set(sparkColor).lerp(boostHeatColor, boostWarmup);
+                    for(int j = 0; j < sparks; j++){
+                        float fin = (Time.time / sparkLife + rand.random(sparkRecurrence + 1f)) % sparkRecurrence;
+                        float or = rand.range(2f);
+                        Tmp.v1.set(sparkRange * fin, 0).rotate(rotdeg() + rand.range(sparkSpread));
+
+                        Draw.color(spark, col, fin);
+                        float px = Tmp.v1.x, py = Tmp.v1.y;
+                        if(fin <= 1f) Lines.lineAngle(lx + px + or * ddx, ly + py + or * ddy, Angles.angle(px, py), Mathf.slope(fin) * sparkSize);
+                    }
+                    Draw.reset();
                 }
             }
+
+            if(glowRegion.found()){
+                Draw.z(Layer.blockAdditive);
+                Draw.blend(Blending.additive);
+                Draw.color(Tmp.c1.set(heatColor).lerp(boostHeatColor, boostWarmup), warmup * (heatColor.a * (1f - heatPulse + Mathf.absin(heatPulseScl, heatPulse))));
+                Draw.rect(glowRegion, x, y, rotdeg());
+                Draw.blend();
+                Draw.color();
+            }
+
+            Draw.blend();
+            Draw.reset();
         }
     }
 }
