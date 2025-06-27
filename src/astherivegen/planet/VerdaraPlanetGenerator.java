3712e3e56c2d6f675c73d4f2bd9344c24d110d00
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 06:28:28 +0700

    Update VerdaraPlanetGenerator.java

--- src/astherivegen/planet/VerdaraPlanetGenerator.java
+++ src/astherivegen/planet/VerdaraPlanetGenerator.java
@@ -20,7 +20,7 @@ public class VerdaraPlanetGenerator extends PlanetGenerator {
 
     float rawHeight(Vec3 position){
         float poles = Math.abs(position.y);
-        if (poles<0.3) {return 0} else return Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, 10f + position.x, 10f + position.y, 10f + position.z);
+        if (poles<0.3) {return 0;} else return Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, 10f + position.x, 10f + position.y, 10f + position.z);
     }
 
     @Override
