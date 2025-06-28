2aa08359d0ad9ee46d55c4380cdbe1fa4067fe28
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 08:49:32 +0700

    Update VerdaraPlanetGenerator.java

--- src/astherivegen/planet/VerdaraPlanetGenerator.java
+++ src/astherivegen/planet/VerdaraPlanetGenerator.java
@@ -23,6 +23,7 @@ public class VerdaraPlanetGenerator extends PlanetGenerator {
         float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z);
         if (poles<0.1f||height<0.6f) {return 0+height*0.2f;} else return height;
     }
+    /*
     @Override
     public Color getColor(Vec3 position){
         Block block = getBlock(position);
@@ -33,5 +34,12 @@ public class VerdaraPlanetGenerator extends PlanetGenerator {
         float height = rawHeight(position);
         Block result = height < 0.5f ? VerdaraEnv.deepWatergel : height < 0.55f ? VerdaraEnv.shallowWatergel : height < 0.6f ? VerdaraEnv.alyogelFloor : height < 0.7f ? VerdaraEnv.eonstoneFloor : height < 0.8f ? VerdaraEnv.eonstoneErodedFloor : Blocks.ferricStone;
         return result;
+    }
+    */
+    //borrowing :3
+    @Override
+    public Color getColor(Vec3 position){
+        Block block = rawHeight(position) < 0.35f ? Blocks.water : rawHeight(position) < 0.4f ? Blocks.sand : rawHeight(position) < 0.64f ? Blocks.grass : Blocks.ferricStone;
+        return Tmp.c1.set(block.mapColor).a(1f - block.albedo);
     }
 }
