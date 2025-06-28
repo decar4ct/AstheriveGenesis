ce6ef342c108b2ef02a8812c9cb7840edd391e3f
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 16:24:50 +0700

    IM SO DONE WITH TS AHHHHHH

--- src/astherivegen/planet/VerdaraPlanetGenerator.java
+++ src/astherivegen/planet/VerdaraPlanetGenerator.java
@@ -23,13 +23,13 @@ public class VerdaraPlanetGenerator extends PlanetGenerator {
 
     float rawHeight(Vec3 position){
         float poles = Math.abs(position.y);
-        float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z)-0.2f;
+        float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z)-0.05f;
         if (poles<0.8f) {
             //any other
             if (poles<0.1f||height<seaLevel) {return seaLevel;} else return height;
         } else {
             //poles specific for ice
-            return height+poles*0.8f;
+            return height+poles*0.2f;
         }
     }
     @Override
