e95e52a79e55006ddfe92b180570959f9c7a542e
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 12:47:19 +0700

    Update ProtusPlanetGenerator.java

--- src/astherivegen/planet/ProtusPlanetGenerator.java
+++ src/astherivegen/planet/ProtusPlanetGenerator.java
@@ -3,32 +3,95 @@ package astherivegen.planet;
 import arc.graphics.*;
 import arc.math.*;
 import arc.math.geom.*;
-import arc.util.Tmp;
+import arc.util.*;
 import arc.util.noise.*;
-import astherivegen.content.Verdara.blocks.VerdaraEnv;
-import mindustry.content.Blocks;
-import mindustry.maps.generators.PlanetGenerator;
-import mindustry.world.Block;
+import mindustry.ai.*;
+import mindustry.content.*;
+import mindustry.game.*;
+import mindustry.maps.generators.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.meta.*;
 
-public class ProtusPlanetGenerator extends PlanetGenerator {
-    public float heightScl = 1f, octaves = 4, persistence = 1.5f, heightPow = 2.2f, heightMult = 1.3f;
+import static mindustry.Vars.*;
 
-    Color out = new Color();
+public class ProtusPlanedGenerator extends PlanetGenerator{
+    public float heightScl = 0.9f, octaves = 8, persistence = 0.7f, heightPow = 3f, heightMult = 1.6f;
+
+    //TODO inline/remove
+    public static float arkThresh = 0.28f, arkScl = 0.83f;
+    public static int arkSeed = 7, arkOct = 2;
+    public static float liqThresh = 0.64f, liqScl = 87f, redThresh = 3.1f, noArkThresh = 0.3f;
+    public static int crystalSeed = 8, crystalOct = 2;
+    public static float crystalScl = 0.9f, crystalMag = 0.3f;
+    public static float airThresh = 0.13f, airScl = 14;
+
+    Block[] terrain = {Blocks.regolith, Blocks.regolith, Blocks.regolith, Blocks.regolith, Blocks.yellowStone, Blocks.rhyolite, Blocks.rhyolite, Blocks.carbonStone};
 
     @Override
     public float getHeight(Vec3 position){
         return Mathf.pow(rawHeight(position), heightPow) * heightMult;
     }
 
-    float rawHeight(Vec3 position){
-        float poles = Math.abs(position.y);
-        float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z);
-        return height+poles*0.2f;
-    }
     @Override
-    public Color getColor(Vec3 position){
+    public void getColor(Vec3 position, Color out){
+        Block block = getBlock(position);
+
+        //more obvious color
+        if(block == Blocks.crystallineStone) block = Blocks.crystalFloor;
+
+        out.set(block.mapColor).a(1f - block.albedo);
+    }
+
+    @Override
+    public float getSizeScl(){
+        return 2000 * 1.07f * 6f / 5f;
+    }
+
+    float rawHeight(Vec3 position){
+        return Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, 10f + position.x, 10f + position.y, 10f + position.z);
+    }
+
+    float rawTemp(Vec3 position){
+        return position.dst(0, 0, 1)*2.2f - Simplex.noise3d(seed, 8, 0.54f, 1.4f, 10f + position.x, 10f + position.y, 10f + position.z) * 2.9f;
+    }
+
+    Block getBlock(Vec3 position){
+        float px = position.x, py = position.y, pz = position.z;
+
+        float ice = rawTemp(position);
         float height = rawHeight(position);
-        Block block = Blocks.water;
-        return (block.mapColor).write(out).a(1f - block.albedo);
+
+        height *= 1.2f;
+        height = Mathf.clamp(height);
+
+        Block result = terrain[Mathf.clamp((int)(height * terrain.length), 0, terrain.length - 1)];
+
+        if(ice < 0.3 + Math.abs(Ridged.noise3d(seed + crystalSeed, px + 4f, py + 8f, pz + 1f, crystalOct, crystalScl)) * crystalMag){
+            return Blocks.crystallineStone;
+        }
+
+        if(ice < 0.6){
+            if(result == Blocks.rhyolite || result == Blocks.yellowStone || result == Blocks.regolith){
+                //TODO bio(?) luminescent stuff? ice?
+                return Blocks.carbonStone; //TODO perhaps something else.
+            }
+        }
+
+        //TODO tweak this to make it more natural
+        //TODO edge distortion?
+        if(ice < redThresh - noArkThresh && Ridged.noise3d(seed + arkSeed, px + 2f, py + 8f, pz + 1f, arkOct, arkScl) > arkThresh){
+            //TODO arkyic in middle
+            result = Blocks.beryllicStone;
+        }
+
+        if(ice > redThresh){
+            result = Blocks.redStone;
+        }else if(ice > redThresh - 0.4f){
+            //TODO this may increase the amount of regolith, but it's too obvious a transition.
+            result = Blocks.regolith;
+        }
+
+        return result;
     }
 }
