47f614745d24e1f20045cae2d2e591b3836e4be8
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-29 06:52:07 +0700

    Update VerdaraEnv.java

--- src/astherivegen/content/Verdara/blocks/VerdaraEnv.java
+++ src/astherivegen/content/Verdara/blocks/VerdaraEnv.java
@@ -143,7 +143,8 @@ public class VerdaraEnv {
                 //ore thing
                 eonstoneQuartzCluster = SteamVent("eonstone-quartz-cluster"){{
                     parent = eonstoneErodedFloor;
-                    effect = none
+                    effect = none;
+                    itemDrop = VerdaraItems.quartz;
                 }};
             }
         }
