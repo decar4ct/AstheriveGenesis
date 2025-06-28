d71e840fa1c2144d9df2c51464ce9438048e164b
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 08:00:43 +0700

    Update VerdaraPlanetGenerator.java

--- src/astherivegen/planet/VerdaraPlanetGenerator.java
+++ src/astherivegen/planet/VerdaraPlanetGenerator.java
@@ -26,8 +26,8 @@ public class VerdaraPlanetGenerator extends PlanetGenerator {
     }
 
     @Override
-    public Color getColor(Vec3 position){
+    public void getColor(Vec3 position){
         Block block = rawHeight(position) < 0.5f ? VerdaraEnv.deepWatergel : rawHeight(position) < 0.55f ? VerdaraEnv.shallowWatergel : rawHeight(position) < 0.6f ? VerdaraEnv.alyogelFloor : rawHeight(position) < 0.7f ? VerdaraEnv.eonstoneFloor : rawHeight(position) < 0.8f ? VerdaraEnv.eonstoneErodedFloor : Blocks.ferricStone;
-        return Tmp.c1.set(block.mapColor).a(1f);
+        out.set(block.mapColor).a(1f - block.albedo);
     }
 }
