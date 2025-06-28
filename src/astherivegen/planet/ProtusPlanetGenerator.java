6666926043963cda1e661e2614de7d6c995bb9bf
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 12:09:01 +0700

    700th commit yay

--- src/astherivegen/planet/ProtusPlanetGenerator.java
+++ src/astherivegen/planet/ProtusPlanetGenerator.java
@@ -13,6 +13,8 @@ import mindustry.world.Block;
 public class ProtusPlanetGenerator extends PlanetGenerator {
     public float heightScl = 1f, octaves = 4, persistence = 1.5f, heightPow = 2.2f, heightMult = 1.3f;
 
+    Color out = new Color();
+
     @Override
     public float getHeight(Vec3 position){
         return Mathf.pow(rawHeight(position), heightPow) * heightMult;
@@ -21,7 +23,7 @@ public class ProtusPlanetGenerator extends PlanetGenerator {
     float rawHeight(Vec3 position){
         float poles = Math.abs(position.y);
         float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z);
-        return height+poles*0.4f;
+        return height+poles*0.2f;
     }
     @Override
     public Color getColor(Vec3 position){
