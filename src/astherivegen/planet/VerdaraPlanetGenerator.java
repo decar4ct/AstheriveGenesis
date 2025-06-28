07aaff2d62befb0a2436ec56642048ebfd7aa9f3
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 08:01:45 +0700

    bruh

--- src/astherivegen/planet/VerdaraPlanetGenerator.java
+++ src/astherivegen/planet/VerdaraPlanetGenerator.java
@@ -12,7 +12,6 @@ import mindustry.world.Block;
 
 public class VerdaraPlanetGenerator extends PlanetGenerator {
     public float heightScl = 2f, octaves = 4, persistence = 0.9f, heightPow = 2.2f, heightMult = 1.3f;
-    seed = 7;
 
     @Override
     public float getHeight(Vec3 position){
