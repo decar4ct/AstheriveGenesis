ea073f73b8fd792487c3e5c5a8078939811d6bec
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 19:45:59 +0700

    Update VerdaraEnv.java

--- src/astherivegen/content/Verdara/blocks/VerdaraEnv.java
+++ src/astherivegen/content/Verdara/blocks/VerdaraEnv.java
@@ -29,7 +29,7 @@ public class VerdaraEnv {
             alyogelDeepFloor,alyogelFloor,
             eonstoneBoulder,eonstoneErodedBoulder,
             smallSporfloreFern, sporfloreFern,
-            sporfloreTree,
+            sporfloreTree, largeSporfloreTree,
             eonstoneHugeVent;
     public static void load() {
         {
