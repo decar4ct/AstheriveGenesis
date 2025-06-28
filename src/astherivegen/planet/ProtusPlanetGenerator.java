abd3f0329a072114c3042a0b72289d41aa110cbe
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-28 12:06:57 +0700

    Create ProtusPlanetGenerator.java

---
+++ src/astherivegen/planet/ProtusPlanetGenerator.java
@@ -0,0 +1,32 @@
+package astherivegen.planet;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.util.Tmp;
+import arc.util.noise.*;
+import astherivegen.content.Verdara.blocks.VerdaraEnv;
+import mindustry.content.Blocks;
+import mindustry.maps.generators.PlanetGenerator;
+import mindustry.world.Block;
+
+public class ProtusPlanetGenerator extends PlanetGenerator {
+    public float heightScl = 1f, octaves = 4, persistence = 1.5f, heightPow = 2.2f, heightMult = 1.3f;
+
+    @Override
+    public float getHeight(Vec3 position){
+        return Mathf.pow(rawHeight(position), heightPow) * heightMult;
+    }
+
+    float rawHeight(Vec3 position){
+        float poles = Math.abs(position.y);
+        float height = Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, position.x, position.y, position.z)-0.5f;
+        return height+poles*0.4f;
+    }
+    @Override
+    public Color getColor(Vec3 position){
+        float height = rawHeight(position);
+        Block block = Blocks.water;
+        return (block.mapColor).write(out).a(1f - block.albedo);
+    }
+}
