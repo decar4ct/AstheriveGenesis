903fca5d0dee5dbc1daff2eda1030fe25f99eb00
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-29 11:45:43 +0700

    Update ClusterDrill.java

--- src/astherivegen/world/blocks/production/ClusterDrill.java
+++ src/astherivegen/world/blocks/production/ClusterDrill.java
@@ -60,7 +60,7 @@ public class ClusterDrill extends Drill{
     @Override
     public boolean canMine(Tile tile){
         if(tile == null || tile.block().isStatic()) return false;
-        Item drops = tile.overlay.drop();
+        Item drops = tile.overlay().drop();
         return drops != null && drops.hardness <= tier && (blockedItems == null || !blockedItems.contains(drops));
     }
 }
