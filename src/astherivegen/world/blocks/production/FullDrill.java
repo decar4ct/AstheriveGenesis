b9ec8deb6684d7de0a5817519b103207f76392a3
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-29 07:17:27 +0700

    Create FullDrill.java

---
+++ src/astherivegen/world/blocks/production/FullDrill.java
@@ -0,0 +1,46 @@
+package astherivegen.world.blocks.production;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.units.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.production.Drill;
+
+import static mindustry.Vars.*;
+
+//a drill that can only work if all its tiles is fully covered with ores
+public class FullDrill extends Drill{
+    @Override
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
+        countOre(tile);
+        if(isMultiblock()){
+            for(Tile other : tile.getLinkedTilesAs(this, tempTiles)){
+                if(canMine(other)){
+                    if(returnCount>=size*size){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }else{
+            return canMine(tile);
+        }
+    }
+}
