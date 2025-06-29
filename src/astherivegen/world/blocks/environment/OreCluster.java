4cb8febe59096167b207a73ed344f251eb84419d
deca <67582961+decar4ct@users.noreply.github.com>
2025-06-29 11:28:42 +0700

    Create OreCluster.java

---
+++ src/astherivegen/world/blocks/environment/OreCluster.java
@@ -0,0 +1,30 @@
+package astherivegen.world.blocks.environment;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.environment.SteamVent;
+
+import static mindustry.Vars.*;
+
+//can't use an overlay for this because it spans multiple tiles
+public class OreCluster extends SteamVent{
+    public SteamVent(String name){
+        super(name);
+        variants = 2;
+        effect = Fx.none;
+        flags = EnumSet.of(BlockFlag.steamVent);
+    }
+    public Item drop(){
+        return this.itemDrop;
+    }
+}
