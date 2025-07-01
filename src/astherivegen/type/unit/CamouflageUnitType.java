6d7723a34c12c43f25ccf046646b8865a60fcd05
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-01 17:27:03 +0700

    Create CamouflageUnitType.java

---
+++ src/astherivegen/type/unit/CamouflageUnitType.java
@@ -0,0 +1,23 @@
+package astherivegen.type.unit;
+
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.world.meta.*;
+import astherivegen.content.Verdara.VerdaraItems;
+import astherivegen.graphics.*;
+import mindustry.entities.abilities.*;
+
+/** BioUnitType but camouflage, yay. */
+public class CamouflageUnitType extends BioUnitType{
+    public CamouflageUnitType(String name){
+        super(name);
+        @Override
+        public void draw(Unit unit){
+            Draw.mixcol(unit.floorOn().mapColor.write(Tmp.c1).mul(0.8f), 1f);
+            super.draw();
+            Draw.reset();
+        }
+    }
+}
