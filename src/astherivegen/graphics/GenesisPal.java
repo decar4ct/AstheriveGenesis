04cc84dae400986a56e6429190c4fa6c9194d27e
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-01 17:07:24 +0700

    Update GenesisPal.java

--- src/astherivegen/graphics/GenesisPal.java
+++ src/astherivegen/graphics/GenesisPal.java
@@ -4,6 +4,6 @@ import arc.graphics.Color;
 
 public class GenesisPal{
     public static Color
-    verdaraOutline = Color.valueOf("30312E");
+    verdaraOutline = Color.valueOf("30312E"),
     bioEonstoneOutline = Color.valueOf("1D1409");
 }
