bc499a1720d7fce55f159966b3c073ce07654494
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-04 14:41:15 +0700

    Update Root.java

--- src/astherivegen/world/blocks/bioplasm/Root.java
+++ src/astherivegen/world/blocks/bioplasm/Root.java
@@ -67,7 +67,7 @@ public class Root extends BioBlock {
     @Override
     public void load(){
         super.load();
-        atlasRegion.split(Core.atlas.find(name+"-atlas"),32,32);
+        atlasRegion[][] = TextureRegion.split(Core.atlas.find(name+"-atlas"),32,32);
         for (int i=0;i<2;i++){
             leafRegion[i]=Core.atlas.find(name+"-leaf"+String.valueOf(i+1));
         }
