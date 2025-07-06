f7022d14169af3491e0e2d37206a7298561d8d12
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-06 18:30:22 +0700

    Update BioSpawner.java

--- src/astherivegen/world/blocks/bioplasm/BioSpawner.java
+++ src/astherivegen/world/blocks/bioplasm/BioSpawner.java
@@ -44,6 +44,7 @@ public class BioSpawner extends BioBlock {
                     spawnProgress = 0;
                     Unit unit = unitType.create(team);
                 }
+                Log.info(spawnProgress);
             }
         }
         @Override
