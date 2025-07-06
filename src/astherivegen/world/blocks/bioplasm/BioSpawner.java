3365bd80299054ebd795c92f2ee466a17d4e694f
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-06 17:47:59 +0700

    Create BioSpawner.java

---
+++ src/astherivegen/world/blocks/bioplasm/BioSpawner.java
@@ -0,0 +1,63 @@
+package astherivegen.world.blocks.bioplasm;
+
+import arc.Core;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.*;
+import arc.util.*;
+import arc.util.io.*;
+import arc.math.geom.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.gen.Building;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.Tile;
+import mindustry.graphics.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.type.*;
+import java.util.Random;
+import astherivegen.graphics.*;
+
+import static mindustry.Vars.*;
+
+public class BioSpawner extends BioBlock {
+    public UnitType unitType;
+    public BioSpawner(String name){
+        super(name);
+        update=true;
+        isRoot=false;
+    }
+    
+    public class BioSpawnerBuild extends BioBuilding {
+        public int spawnProgress = 0;
+        
+        @Override
+        public void updatePulse(){
+            if (true) {
+                spawnProgress++;
+                if(spawnProgress >= 20) {
+                    spawnProgress = 0;
+                    unitType.create(team);
+                }
+            }
+        }
+        @Override
+        public void draw(){
+            drawPulse(block.region,drawPulseScale);
+        }
+        @Override
+        public void write(Writes write){
+            super.write(write);
+            write.i(spawnProgress);
+        }
+
+        @Override
+        public void read(Reads read, byte revision){
+            super.read(read, revision);
+            spawnProgress=read.i();
+        }
+    }
+ }     
