5e044076bc724d8b8a1fbe446077d6e1d4191965
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-07 07:03:32 +0700

    Create VerdaraLiquid.java

---
+++ src/astherivegen/content/Verdara/blocks/VerdaraLiquid.java
@@ -0,0 +1,29 @@
+package astherivegen.content.Verdara.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.distribution.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import mindustry.content.*;
+import astherivegen.world.blocks.liquid.*;
+import astherivegen.content.Verdara.VerdaraItems;
+
+import static mindustry.type.ItemStack.with;
+
+public class VerdaraDistribution {
+    public static Block
+            pipe;
+    public static void load() {
+        {
+            {
+                pipe = new LiquidPipe("pipe"){{
+                    requirements(Category.liquid, with(VerdaraItems.quartz, 2));
+                }};
+            }
+        }
+    }
+}
