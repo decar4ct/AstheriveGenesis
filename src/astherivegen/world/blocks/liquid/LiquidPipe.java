509efe737854e531bc1314ba41a19ea0c5b8ae6e
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-07 20:42:24 +0700

    Update LiquidPipe.java

--- src/astherivegen/world/blocks/liquid/LiquidPipe.java
+++ src/astherivegen/world/blocks/liquid/LiquidPipe.java
@@ -106,7 +106,7 @@ public class LiquidPipe extends LiquidBlock{
 
         boolean blends(Tile other){
             if(other != null && other.build != null){
-                if(other.build instanceof PipeBuild || other.build instanceof PipePumpBuild){
+                if(other.build instanceof LiquidPipeBuild || other.build instanceof PipePumpBuild){
                     return true;
                 }
             }
@@ -124,7 +124,7 @@ public class LiquidPipe extends LiquidBlock{
 
         @Override
         public boolean acceptLiquid(Building source, Liquid liquid){
-            return (source instanceof PipeBuild || source instanceof PipePumpBuild) && (liquids.current() == liquid || liquids.currentAmount() < 0.2f);
+            return (source instanceof LiquidPipeBuild || source instanceof PipePumpBuild) && (liquids.current() == liquid || liquids.currentAmount() < 0.2f);
         }
     }
 }
