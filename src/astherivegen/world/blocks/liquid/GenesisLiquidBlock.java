110b32381c3936027e24b6efea4753e42b3d6aec
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-07 20:47:48 +0700

    epic

---
+++ src/astherivegen/world/blocks/liquid/GenesisLiquidBlock.java
@@ -0,0 +1,12 @@
+package astherivegen.world.blocks.liquid;
+
+import mindustry.world.blocks.liquid;
+
+public class GenesisLiquidBlock extends LiquidBlock{
+    public GenesisLiquidBlock(String name){
+        super(name);
+    }
+    public class GenesisLiquidBuild {
+        //theres literally nothing here lmao, youve been trolled
+    }
+}
