77b4dfb10e1aa6cd0af4ca2b1ed1794d48a542f5
deca <67582961+decar4ct@users.noreply.github.com>
2025-07-08 15:42:23 +0700

    Update VerdaraDistribution.java

--- src/astherivegen/content/Verdara/blocks/VerdaraDistribution.java
+++ src/astherivegen/content/Verdara/blocks/VerdaraDistribution.java
@@ -16,25 +16,28 @@ import static mindustry.type.ItemStack.with;
 
 public class VerdaraDistribution {
     public static Block
-            itemTube, itemOverpass, filter, splitter;
+            itemTube, itemOverpass, filter, splitter, itemGate;
     public static void load() {
         {
             {
                 itemTube = new ItemTube("item-tube"){{
                     requirements(Category.distribution, with(VerdaraItems.quartz, 1));
-                    speed = 0.05f;
-                    bridgeReplacement=itemOverpass;
+                    speed = 0.065f;
+                    displayedSpeed = 9f;
+                    rotBridgeReplacement=itemOverpass;
                 }};
                 itemOverpass = new ItemOverpass("item-overpass"){{
                     requirements(Category.distribution, with(VerdaraItems.quartz, 6, VerdaraItems.magnetite, 3));
                     range=4;
                 }};
                 filter = new Sorter("filter"){{
-                    requirements(Category.distribution, with(VerdaraItems.quartz, 1));
+                    requirements(Category.distribution, with(VerdaraItems.quartz, 4, VerdaraItems.magnetite, 2));
                 }};
                 splitter = new Router("splitter"){{
-                    requirements(Category.distribution, with(VerdaraItems.quartz, 1));
+                    requirements(Category.distribution, with(VerdaraItems.quartz, 5));
                 }};
+                itemGate = new OverflowGate("item-gate"){{
+                    requirements(Category.distribution, with(VerdaraItems.quartz, 4, VerdaraItems.magnetite, 2));
             }
         }
     }
