03eb26a9f8ad6c0661ed39fd43aa86d0824aa332
decar4ct <aleronp3@gmail.com>
2025-11-08 07:48:43 +0700

    piston pump

--- src/astherivegen/content/Verdara/blocks/VerdaraLiquid.java
+++ src/astherivegen/content/Verdara/blocks/VerdaraLiquid.java
@@ -3,6 +3,7 @@ package astherivegen.content.Verdara.blocks;
 import arc.graphics.Color;
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.production.*;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.content.*;
@@ -20,6 +21,23 @@ public class VerdaraLiquid{
     public static void load() {
         {
             {
+                pistonPump = new Pump("piston-pump"){{
+                    requirements(Category.liquid, with(VerdaraItems.quartz, 25, VerdaraItems.magnetite, 10));
+                    
+                    drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawPumpLiquid(), new DrawPistons(){{
+                    sinMag = 2.75f;
+                    sinScl = 5f;
+                    sides = 4;
+                    sideOffset = (float) Math.PI / 2f;
+                    angleOffset = 45f;
+                    }}, new DrawDefault());
+                    size = 2;
+                    researchCostMultiplier = 0.05f;
+                    consumePower(10/60f);
+                    hasPower = true;
+                    liquidCapacity = 20f;
+                    pumpAmount = 8/60f;
+                }}
                 pipe = new LiquidPipe("pipe"){{
                     requirements(Category.liquid, with(VerdaraItems.quartz, 2));
                 }};
