4f5c854f086bbd822b6a4bd5658d817eb70ad409
decar4ct <aleronp3@gmail.com>
2025-11-10 11:29:26 +0700

    sprites

--- src/astherivegen/content/Verdara/blocks/VerdaraUnits.java
+++ src/astherivegen/content/Verdara/blocks/VerdaraUnits.java
@@ -24,8 +24,8 @@ public class VerdaraUnits {
             {
                 mechAssembler = new UnitFactory("mech-assembler"){{
                     requirements(Category.units, with(VerdaraItems.polterite, 15, VerdaraItems.quartz, 30, VerdaraItems.magnetite,25));
-                    plans.add(new UnitPlan(VerdaraUnitTypes.saber, 12 * 60f, with(VerdaraItems.polterite, 15, VerdaraItems.quartz, 10)));
-                    consumePower(10/60f);
+                    plans.add(new UnitPlan(VerdaraUnitTypes.saber, 15 * 60f, with(VerdaraItems.polterite, 15, VerdaraItems.quartz, 10)));
+                    consumePower(60/60f);
                     size = 3;
                 }};
             }
