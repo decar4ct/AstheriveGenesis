be0a04be4f2697a1c39a0a12a8fc801a1abd3327
decar4ct <aleronp3@gmail.com>
2025-11-10 08:14:51 +0700

    unitfactory

---
+++ src/astherivegen/content/Verdara/blocks/VerdaraUnits.java
@@ -0,0 +1,36 @@
+package astherivegen.content.Verdara.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.units.*;
+import mindustry.world.draw.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import astherivegen.world.blocks.units.*;
+import astherivegen.content.*;
+import astherivegen.content.Verdara.VerdaraItems;
+import astherivegen.contebt.Verdara.*;
+import mindustry.content.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class VerdaraDefense {
+    public static Block
+            mechAssembler;
+    public static void load() {
+        {
+            {
+                mechAssembler = new UnitFactory("mech-assembler"){{
+                    requirements(Category.units, with(VerdaraItems.polterite, 15, VerdaraItems.quartz 30, VerdaraItems.magnetite,25
+                    ));
+                    plans.add(new UnitPlan(VerdaraUnitTypes.saber, 12 * 60f, with(VerdaraItems.polterite, 15, VerdaraItems.quartz, 10)));
+                    consumePower(10/60f);
+                    size = 3;
+                }};
+            }
+        }
+    }
+}
